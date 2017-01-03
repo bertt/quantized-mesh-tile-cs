a67ec54d529146172ca19a00e572b5ae2b88f4aa
bert <btemme@hotmail.com>
2017-01-03 09:49:44 +0100

    adding benchmark test, but getting memoryexception somehow

---
+++ quantized-mesh-tile-cs.benchmark/ParsingBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.IO;
+using System.Reflection;
+using Terrain.Tiles;
+
+namespace quantized_mesh_tile_cs.benchmark
+{
+    public class ParsingBenchmark
+    {
+        Stream input;
+
+        public ParsingBenchmark()
+        {
+            const string firstTerrainFile = "benchmark.data.9_533_383.terrain";
+            input = Assembly.GetExecutingAssembly().GetManifestResourceStream(firstTerrainFile);
+        }
+
+        [Benchmark]
+        public TerrainTile ParseVectorTileFromStream()
+        {
+            return TerrainTileParser.Parse(input);
+        }
+    }
+}
---
+++ quantized-mesh-tile-cs.benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace quantized_mesh_tile_cs.benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ParsingBenchmark>();
+            Console.ReadKey();
+        }
+    }
+}

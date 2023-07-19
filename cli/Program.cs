ce60d5b5344e34a5d6c8e0e4c063e618c845e46f
Bert Temme <btemme@hotmail.com>
2023-07-19 18:49:35 +0200

    update commandline

--- cli/Program.cs
+++ cli/Program.cs
@@ -8,6 +8,10 @@ internal class Program
     static async Task<int> Main(string[] args)
     {
         Console.WriteLine("Terrain Tile - CLI");
+        if (args.Length == 0)
+        {
+            args = new string[] { "-h" };
+        }
 
         var fileOption = new Option<FileInfo?>(
             name: "-i",
@@ -27,14 +31,18 @@ internal class Program
 
     static void ReadFile(FileInfo file)
     {
-        var pbfStream = File.OpenRead(file.Name);
-        var terrainTile = TerrainTileParser.Parse(pbfStream);
+        if (file != null)
+        {
+            var pbfStream = File.OpenRead(file.Name);
+            var terrainTile = TerrainTileParser.Parse(pbfStream);
 
-        Console.WriteLine("Number of vertices: " + terrainTile.VertexData.vertexCount);
-        Console.WriteLine("Minimum height: " + terrainTile.Header.MinimumHeight);
-        Console.WriteLine("Maximum height: " + terrainTile.Header.MaximumHeight);
-        Console.WriteLine("Has normals extension: " + terrainTile.HasNormals);
-        Console.WriteLine("Has watermask extension: " + terrainTile.HasWatermask);
-        Console.WriteLine("Has metadata extension: " + terrainTile.HasMetadata);
+            Console.WriteLine("Number of vertices: " + terrainTile.VertexData.vertexCount);
+            Console.WriteLine("Minimum height: " + terrainTile.Header.MinimumHeight);
+            Console.WriteLine("Maximum height: " + terrainTile.Header.MaximumHeight);
+            Console.WriteLine("Has normals extension: " + terrainTile.HasNormals);
+            Console.WriteLine("Has watermask extension: " + terrainTile.HasWatermask);
+            Console.WriteLine("Has metadata extension: " + terrainTile.HasMetadata);
+
+        }
     }
 }
